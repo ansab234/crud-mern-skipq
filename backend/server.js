0e95c9c051db9f8db26058ec2f96a5320c10ef57
ansab234 <ansabali067@gmail.com>
2022-06-18 11:09:23 +0500

    deployment

--- backend/server.js
+++ backend/server.js
@@ -20,23 +20,23 @@ app.use(express.json());
 app.use("/", urlRoutes);
 app.use("/", userRoutes);
 
-// -----------------deployment-----------------
+// --------------------------deployment------------------------------
 
-__dirname = path.resolve();
+const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
-  })
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
 }
 
-// -----------------deployment-----------------
+// --------------------------deployment------------------------------
 
 // Error Handling middlewares
 app.use(notFound);
