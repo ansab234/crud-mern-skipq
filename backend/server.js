001ccd534e34cdbb8bbcc0eed61ddebf2cd3d740
ansab234 <ansabali067@gmail.com>
2022-06-18 18:22:09 +0500

    without-deployment

--- backend/server.js
+++ backend/server.js
@@ -22,19 +22,19 @@ app.use("/", userRoutes);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/hy", (req, res) => {
-    res.send("API is running..");
- });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/hy", (req, res) => {
+//     res.send("API is running..");
+//  });
+// }
 
 // --------------------------deployment------------------------------
 
