e1ed7da47d22593313c2e6541d6d3ccc3a36b1a6
ansab234 <ansabali067@gmail.com>
2022-06-18 12:06:15 +0500

    deployment

--- backend/server.js
+++ backend/server.js
@@ -22,13 +22,13 @@ app.use("/", userRoutes);
 
 // --------------------------deployment------------------------------
 
-// const __dirname1 = path.resolve();
+const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "build")));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/hy", (req, res) => {
